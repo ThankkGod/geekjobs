3f689f0a55269a89bd292f2028bf2cadf42c825e
unknown <sundaythankgo56@gmail/.com>
2026-09-16 08:07:40 +0100

    Fix production admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,7 +13,7 @@ class AdminSeeder extends Seeder
      */    public function run(): void
     {
    
-        $user = User::create([
+        $user = User::updateOrCreate([
             'name' => 'Geeky ThankGod',
             'email' => 'geeky@gmail.com',
             'password' => bcrypt('geeky@gmail.com'),
